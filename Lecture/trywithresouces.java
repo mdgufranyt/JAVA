333ad25b4788cd0df9d809b5e4f82472a57474bb
mdgufranyt <gufranbareilly@gmail.com>
2024-12-25 20:17:59 +0530

    8:17

--- Lecture/trywithresouces.java
+++ Lecture/trywithresouces.java
@@ -1,44 +1,46 @@
 package Lecture;
+
 import java.io.*;
 import java.util.*;
-
 
 public class trywithresouces {
 
     static FileInputStream fi;
     static Scanner sc;
 
-
     static void Divide() throws Exception
     {
-        fi=new FileInputStream("/Users/moham/Desktop");
 
-        sc = new Scanner(fi);
+        try(FileInputStream fi=new FileInputStream("/Users/moham/Desktop"); 
+        {
+            sc = new Scanner(fi);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
         System.out.println(a/c);
 
-        fi.close();
+
+
+        }
+    
+
+    
        
     }
-    
-    
 
     public static void main(String[] args) throws Exception {
 
-        try{
+        try {
             Divide();
 
-        }
-        catch(Exception e){
+        } catch (Exception e) {
             System.out.println(e);
         }
-        
+
         int x = sc.nextInt();
 
         System.out.println(x);
-        
+
     }
-    
+
 }
