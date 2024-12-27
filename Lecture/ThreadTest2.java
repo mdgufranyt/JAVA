81f158802a4a741e32eb12557506ae23cc4dd27c
mdgufranyt <gufranbareilly@gmail.com>
2024-12-27 23:23:28 +0530

    threads with runnable interface

---
+++ Lecture/ThreadTest2.java
@@ -0,0 +1,27 @@
+package Lecture;
+
+public class ThreadTest2 implements Runnable {
+
+    public void run() {
+        int i = 1;
+        while (true) {
+            System.out.println(i + "Hello");
+            i++;
+        }
+
+    }
+
+    public static void main(String[] args) {
+        ThreadTest2 m = new ThreadTest2();
+        Thread t = new Thread(m);
+        t.start();
+
+        int i = 1;
+        while (true) {
+            System.out.println(i + "World");
+            i++;
+        }
+
+    }
+
+}
